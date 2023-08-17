10ab3e3ca6334cab4f1c086bc356dfec4ce58c67
Andrew-seatter <135391186+Andrew-seatter@users.noreply.github.com>
2023-08-17 03:45:16 -0700

    changes

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,13 @@
-var topics = "HTML, CSS, Git, JavaScript";
-console.log(topics);
+var topics = "HTML";
+
+if (topics === 'HTML') {
+    console.log("Let's study HTML!");
+} else if (topics === 'CSS') {
+    console.log("Let's study CSS");
+} else if (topics === 'Git') {
+    console.log("Let's study Git!");
+} else if (topics === 'Javascript') {
+    console.log("Let's study Javascript");
+} else {
+    console.log('Please try again!');
+}
